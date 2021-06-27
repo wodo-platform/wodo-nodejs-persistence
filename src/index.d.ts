d549ff27f834c7b13cbe304e456d2c0afdd1480f
Serhat Tanrikut <seta@OSX-SETA-N-BE.local>
2021-06-27 20:05:24 +0200

    initial commit

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+
+import { PrismaClient } from "@prisma/client";
+
+export function createContext(ctx: any): Promise<Context>;
+
+export * from ".prisma/client/index.d";
+
+export interface Context {
+  prisma: PrismaClient;
+}
