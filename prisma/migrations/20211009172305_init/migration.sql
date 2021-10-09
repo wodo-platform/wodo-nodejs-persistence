6afe4864c9e9a0cb274fea7254d9f343e217635f
Serhat Tanrikut <serhat.tanrikut@gmail.com>
2021-10-09 19:34:20 +0200

    https://github.com/wodo-platform/wg-management/issues/17 Shared persistance nodejs module

--- prisma/migrations/20211009172305_init/migration.sql
+++ prisma/migrations/20211009172305_init/migration.sql
@@ -1,8 +1,13 @@
 -- CreateTable
-CREATE TABLE `Test` (
+CREATE TABLE `Demo` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191),
+    `deleted` BOOLEAN NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+    UNIQUE INDEX `Demo_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
